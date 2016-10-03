2c89b25e990d6e0f5da9e172a27feeef764dd931
hj222hi <hj222hi@student.lnu.se>
2016-10-03 16:39:42 +0200

    Mer på 1

--- src/hj222hi/MyGraph.java
+++ src/hj222hi/MyGraph.java
@@ -3,42 +3,77 @@ package hj222hi;
 import graphs.DirectedGraph;
 import graphs.Node;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+
+import java.util.*;
 
 /**
  * Created by hampus on 2016-09-30.
  */
-public class MyGraph implements DirectedGraph{
-    private MyGraph graph;
+public class MyGraph<E> implements DirectedGraph{
+
+    private Map<E, MyNode<E>> _graph = new HashMap<E, MyNode<E>>();
+    private Set<MyNode<E>> _heads = new HashSet<MyNode<E>>();
+    private Set<MyNode<E>> _tails = new HashSet<MyNode<E>>();
     public MyGraph(){
-        graph =  new MyGraph();
+        _graph = new HashMap<>();
+        _heads = new HashSet<>();
+        _tails = new HashSet<>();
     }
 
     @Override
-    public Node addNodeFor(Object item) {
-        return null;
+    public MyNode addNodeFor(E item) {
+        //Fell exception BRE
+        if(item == null){ throw new IndexOutOfBoundsException();}
+
+        if(!_graph.containsKey(item)){
+            MyNode<E> node = new MyNode<E>(item);
+            _heads.add(node);
+            _tails.add(node);
+            _graph.put(item, node);
+        }
+
     }
 
     @Override
-    public Node getNodeFor(Object item) {
-        return null;
+    public MyNode getNodeFor(E item) {
+        if (item != null && !_graph.containsKey(item)){
+            return _graph.get(item);
+        }
+        throw new IndexOutOfBoundsException();
     }
 
     @Override
-    public boolean addEdgeFor(Object from, Object to) {
+    public boolean addEdgeFor(E from, E to) {
+
+        if(from != null || to != null){
+            MyNode From = (MyNode<E>) addNodeFor(from);
+            MyNode To = (MyNode<E>) addNodeFor(to);
+
+            if(From.hasSucc(To)){
+                return false;
+            }else {
+                From.addSucc(To);
+                To.addPred(From);
+
+                _tails.remove(From);
+                _heads.remove(To);
+
+            }
+        }else {
+            throw new IndexOutOfBoundsException();
+        }
+
         return false;
     }
 
     @Override
-    public boolean containsNodeFor(Object item) {
-        return false;
+    public boolean containsNodeFor(E item) {
+        return _graph.containsKey(item) || _graph.containsValue(item);
     }
 
     @Override
     public int nodeCount() {
-        return 0;
+        return _graph.size();
     }
 
     @Override
