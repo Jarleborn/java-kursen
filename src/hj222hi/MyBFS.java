9a107a855aba5806c0bff067914c87520227910f
hj222hi <hj222hi@student.lnu.se>
2016-10-10 04:40:47 +0200

    Got to speed up this... BFS soon done

--- src/hj222hi/MyBFS.java
+++ src/hj222hi/MyBFS.java
@@ -4,8 +4,10 @@ import graphs.BFS;
 import graphs.DirectedGraph;
 import graphs.Node;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
-
+import java.lang.Integer;
 /**
  * Created by hampus on 2016-10-05.
  */
@@ -18,5 +20,33 @@ public class MyBFS<Integer> implements BFS {
     @Override
     public List<Node> bfs(DirectedGraph graph) {
         return null;
+    }
+
+    public List<Node<Integer>> bfsRecursive(List<Node<Integer>> returningNodes, HashSet<Node<Integer>> hash, HashSet<Node<Integer>> visited){
+
+
+        Iterator<Node<Integer>> visIterator = hash.iterator();
+        HashSet<Node<Integer>> newHash = new HashSet<>();
+
+
+        while (visIterator.hasNext()) {
+            Node<Integer> n = visIterator.next();
+            if (!visited.contains(n)) {
+                n.num = visited.size();
+                visited.add(n);
+                returningNodes.add(n);
+            }
+
+            Iterator<Node<Integer>> succs = n.succsOf();
+            while(succs.hasNext()){
+                Node<Integer> newN = succs.next();
+                if(!returningNodes.contains(newN)){
+                    newHash.add(newN);
+                }
+            }
+
+        }
+
+        return returningNodes;
     }
 }
